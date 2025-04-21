5eeb26b440b50fbfc68b4999cad979cdb190513a
phlong19 <phl19.dev@gmail.com>
2025-04-22 00:45:18 +0700

    chore: outputting content in angular

--- fe/src/app/components/posts/post-create/post-create.component.ts
+++ fe/src/app/components/posts/post-create/post-create.component.ts
@@ -4,8 +4,11 @@ import { Component } from '@angular/core';
   selector: 'app-post-create',
   imports: [],
   templateUrl: './post-create.component.html',
-  styleUrl: './post-create.component.scss'
+  styleUrl: './post-create.component.scss',
 })
 export class PostCreateComponent {
-
+  content = '';
+  onAddPost() {
+    this.content = "this user's post";
+  }
 }
