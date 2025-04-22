61f4806d24f2f84a6aa8078c2f2355a9b9bc161d
phlong19 <phl19.dev@gmail.com>
2025-04-23 01:21:46 +0700

    chore: added angular material

--- fe/src/app/components/posts/post-create/post-create.component.ts
+++ fe/src/app/components/posts/post-create/post-create.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-post-create',
-  imports: [],
+  imports: [FormsModule, MatCardModule, MatInputModule, MatButtonModule],
   templateUrl: './post-create.component.html',
   styleUrl: './post-create.component.scss',
 })
 export class PostCreateComponent {
-  content = '';
-  onAddPost() {
-    this.content = "this user's post";
+  content = 'no content';
+
+  onAddPost(postInput: HTMLTextAreaElement) {
+    this.content = postInput.value;
+  }
+
+  enteredValue = '';
+  onCreatePost() {
+    this.content = this.enteredValue;
   }
 }
