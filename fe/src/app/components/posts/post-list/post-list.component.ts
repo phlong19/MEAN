303cd950bc61986418afb1f9680d0d88620f0664
phlong19 <phl19.dev@gmail.com>
2025-04-27 19:12:13 +0700

    chore: structural directive for & if

--- fe/src/app/components/posts/post-list/post-list.component.ts
+++ fe/src/app/components/posts/post-list/post-list.component.ts
@@ -1,10 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatExpansionModule } from '@angular/material/expansion';
 
+interface Post {
+  title: string;
+  content: string;
+}
 @Component({
   selector: 'app-post-list',
-  imports: [MatExpansionModule],
+  imports: [CommonModule, MatExpansionModule],
   templateUrl: './post-list.component.html',
   styleUrl: './post-list.component.scss',
 })
-export class PostListComponent {}
+export class PostListComponent {
+  posts: Post[] = [];
+}
