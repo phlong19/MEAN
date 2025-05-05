6bd95782429dd8c061d4ae6ccbc5985e8132c418
phlong19 <phl19.dev@gmail.com>
2025-05-06 00:44:40 +0700

    feat: config app and add new api endpoint for fetching posts

--- src/backend/app.ts
+++ src/backend/app.ts
@@ -1,10 +1,22 @@
 import express from 'express';
+import { Post } from '../app/post.model';
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.end('say hi');
+app.get('/posts', (req, res, next) => {
+  const posts: Post[] = [
+    {
+      content: 'instagram chat call in 56 minutes.',
+      title: 'some random measurement.',
+    },
+    { content: 'Other content for testing display.', title: 'Random title!' },
+  ];
+  res.status(200).json({
+    code: 200,
+    message: 'Fetched posts successfully!',
+    posts,
+  });
+
   next();
 });
-
 export default app;
--- src/backend/server.ts
+++ src/backend/server.ts
@@ -4,5 +4,25 @@ import app from './app';
 const port: number = Number(process.env['PORT']) || 3000;
 
 const server = http.createServer(app);
+const address = server.address();
 
+const onError = (error: any) => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const bind =
+    typeof address === 'string' ? 'pipe: ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.log(bind + ' requires elevated privileges.');
+      process.exit(1);
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+    default:
+      break;
+  }
+};
+
+server.on('error', onError);
 server.listen(port);
